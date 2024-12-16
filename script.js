ffa4fa33b3f517e45e3c93cdcf0205d61faac301
warmdev17 <warmdevofficial@gmail.com>
2024-12-17 06:20:55 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Scroll Animation
+const animatedElements = document.querySelectorAll(".animate");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.3,
+  },
+);
+
+animatedElements.forEach((element) => {
+  observer.observe(element);
+});
